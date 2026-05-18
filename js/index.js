a6fe66bc74bf4a6526c51e80ee11088d2d1c9fe0
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-05-18 01:24:14 -0500

    Added css and js files and added more html pages

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(() => {
+    document.querySelector("#testButton").addEventListener("click", handleClick);
+
+    function handleClick() {
+        console.log("Thank you for clicking.");
+    }
+})();
