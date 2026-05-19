8222599ba67e417a56d6fab228e8555562024c5a
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-05-19 14:56:30 -0500

    attempted to set up Contact Form.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+// app.get('/', (req, res) => {
+// res.send("Hello World!");
+// });
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+// app.post('/mail', (req, res) => {
+//  console.log("mail button clicked");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ public/js/contact.js
@@ -0,0 +1,17 @@
+(function () {
+ "use strict";
+
+    document
+        .querySelector("#contact-form-button")
+        .addEventListener("click", submitMail);
+
+    function submitMail() {
+        console.log("YOU CLICKED THE SUBMIT BUTTON.");
+        let name = document.querySelector("#name").value;
+        let email = document.querySelector("#mail").value;
+        let message = document.querySelector("#msg").value;
+        console.log("Name" + name);
+        console.log("Email" + email);
+        console.log("Message" + message);
+    }
+})();
