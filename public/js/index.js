3f49d2a6956001ad05d3a7d80ad434bf1be7607b
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-05-19 03:24:45 -0500

    set up Bootstrap into the index.html, added a new Google font and a header.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
+(function(){
 "use strict";
 
-(() => {
+
     document.querySelector("#testButton").addEventListener("click", handleClick);
 
     function handleClick() {
         console.log("Thank you for clicking.");
     }
+    
 })();
