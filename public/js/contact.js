b8e448af2b7b89f69b3e5b18b9974bc68c58de88
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-05-19 17:08:56 -0500

    removed 'function submitMail()' from contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -4,16 +4,15 @@
     document
         .querySelector("#contact-form-button")
         .addEventListener("click", (event) =>{
-        event.preventDefault();
-        event.stopPropagation();
-        console.log("YOU CLICKED THE SUBMIT BUTTON.");
-        let name = document.querySelector("#name").value;
-        let email = document.querySelector("#mail").value;
-        let message = document.querySelector("#msg").value;
-        console.log("Name" + name);
-        console.log("Email" + email);
-        console.log("Message" + message);
-    });
-
-    function submitMail() 
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("YOU CLICKED THE SUBMIT BUTTON.");
+            let name = document.querySelector("#name").value;
+            let email = document.querySelector("#mail").value;
+            let message = document.querySelector("#msg").value;
+            console.log("Name" + name);
+            console.log("Email" + email);
+            console.log("Message" + message);
+        });
+ 
 })();
