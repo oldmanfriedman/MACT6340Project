14bb28434ecf094e49fdad08b88f82bf0dfd63a0
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-05-30 00:59:35 -0500

    Converted the .html pages into .ejs. Re-linked all pages.

--- utils/utils.js
+++ utils/utils.js
@@ -2,14 +2,11 @@ import nodemailer from "nodemailer";
 
 export async function sendMessage(sub, txt) {
     let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
-            secure: process.env.MAIL_SECURE,
+        service: process.env.MAIL_SERVICE,
             auth: {
                 user: process.env.MAIL_USERNAME,
                 pass: process.env.MAIL_PASSWORD,
             },
-            requireTLS: process.env.MAIL_TLS,
         });
 
 let message = {
