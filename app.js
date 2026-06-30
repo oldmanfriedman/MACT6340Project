f22bb586151801b2f9c343b19bef6c35b58f4624
oldmanfriedman <229003189+oldmanfriedman@users.noreply.github.com>
2026-06-29 22:45:35 -0500

    Added updates to the app.js file.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js"
 dotenv.config();
+import * as db from './utils/database.js';
 let data = ["Project 1", "Project 2", "Project 3"];
+let projects = [];
 
 const app = express();
 const port = 3000;
@@ -10,12 +12,20 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-res.render("index.ejs");
+app.get("/", async (req, res) => {
+  await db
+  .connect()
+  .then(async() => {
+    // query the database for project records
+    projects = await db.getAllProjects();
+    console.log(projects);
+    res.render("index.ejs");
+  })
+  .catch(next);
 });
 
-app.get('/projects', (req, res) => {
-  res.render("projects.ejs", { projectArray: data });
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs", {data: projects});
 });
 
 app.get("/project/:id", (req, res) => {
